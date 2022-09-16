6766c9c46caf619a1212cece093d611bca7d8f8a
SvenST89 <svensteinbauer89@googlemail.com>
2022-09-16 14:43:10 +0200

    First push for new repository 'Valuation Model' on branch develop

---
+++ ValuationModel/.ipynb_checkpoints/pw-checkpoint.py
@@ -0,0 +1,1 @@
+password="FINDSMaster89"
---
+++ ValuationModel/assist_functions.py
@@ -0,0 +1,29 @@
+# Function for swapping columns to fit the database schema
+def swap_columns(df, col1, col2):
+    col_list = list(df.columns)
+    x, y = col_list.index(col1), col_list.index(col2)
+    col_list[y], col_list[x] = col_list[x], col_list[y]
+    df = df[col_list]
+    return df
+
+# Function to write Data to a table in database
+import psycopg2.extras as extras
+def execute_values(conn, df, table):
+  
+    tuples = [tuple(x) for x in df.to_numpy()]
+  
+    cols = ','.join(list(df.columns))
+  
+    # SQL query to execute
+    query = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("execute_values() done")
+    cursor.close()
